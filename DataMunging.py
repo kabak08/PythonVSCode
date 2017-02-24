83953051b254a4f846764220a3723e7f882d0be6
kabak08 <kabak08@gmail.com>
2017-02-24 09:15:12 -0500

    initial commit

---
+++ DataMunging.py
@@ -0,0 +1,65 @@
+import pandas as pd
+import os
+
+url = "https://data.boston.gov/dataset/6220d948-eae2-4e4b-8723-2dc8e67722a3/resource/12cb3883-56f5-47de-afa5-3b1cf61b257b/download/crime.csv"
+
+data = pd.read_csv(url)
+len(data) #shows us we have 161,127 rows of data
+data.head #shows us the top efew rows of our data
+names = data.columns.values #shows us the column names
+list(names)
+
+
+from bokeh.io import output_file, show
+from bokeh.models import GeoJSONDataSource
+from bokeh.plotting import figure
+from bokeh.sampledata.sample_geojson import geojson
+
+
+
+
+geo_source = GeoJSONDataSource(geojson=geojson)
+
+p = figure()
+p.circle(x='x', y='y', alpha=0.9, source=geo_source)
+output_file("geojson.html")
+show(p)
+
+
+
+
+#Map data ©2017 Google
+#Terms of Use
+#Report a map error
+from bokeh.io import output_file, show
+from bokeh.models import (
+  GMapPlot, GMapOptions, ColumnDataSource, Circle, DataRange1d, PanTool, WheelZoomTool, BoxSelectTool
+)
+
+map_options = GMapOptions(lat=42.3601, lng=-71.0589, map_type="roadmap", zoom=11)
+
+plot = GMapPlot(
+    x_range=DataRange1d(), y_range=DataRange1d(), map_options=map_options
+)
+plot.title.text = "Boston"
+
+# For GMaps to function, Google requires you obtain and enable an API key:
+#
+#     https://developers.google.com/maps/documentation/javascript/get-api-key
+#
+# Replace the value below with your personal API key:
+plot.api_key = "AIzaSyB1IR_MczcjRvimyTR7wmD8aQgpKbUUBjA"
+
+source = ColumnDataSource(
+    data=dict(
+        lat=data["Lat"],
+        lon=data["Long"],
+    )
+)
+
+circle = Circle(x="lon", y="lat", size=15, fill_color="blue", fill_alpha=0.8, line_color=None)
+plot.add_glyph(source, circle)
+
+plot.add_tools(PanTool(), WheelZoomTool(), BoxSelectTool())
+output_file("gmap_plot.html")
+show(plot)
---
+++ Test.py
@@ -0,0 +1,7 @@
+import pyodbc
+import pandas as pd
+import scikit-learn as 
+
+data = pd.read_csv('/Users/kabak08gmail.com/Desktop/PythonVSCode/globalterrorismdb_0616dist.csv')
+
+
